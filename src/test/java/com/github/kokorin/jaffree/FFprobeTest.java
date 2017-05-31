df4e8387d2fadf0dcaa49d4cb92830344e5071ba
kokorin <kokorin86@gmail.com>
2017-05-31 08:18:12 +0300

    Remove hardcoded paths from tests

--- src/test/java/com/github/kokorin/jaffree/FFprobeTest.java
+++ src/test/java/com/github/kokorin/jaffree/FFprobeTest.java
@@ -4,19 +4,34 @@ import com.github.kokorin.jaffree.cli.StreamSpecifier;
 import com.github.kokorin.jaffree.ffprobe.xml.FFprobeType;
 import com.github.kokorin.jaffree.ffprobe.xml.StreamType;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FFprobeTest {
-    public static final Path bin = Paths.get("D:\\Projects\\ffmpeg\\bin");
-    public static final Path input = Paths.get("D:\\Projects\\Jaffree\\target\\samples\\MPEG-4\\video.mp4");
+    public static Path bin;
+    public static Path samples = Paths.get("target/samples");
+    public static Path videoMP4 = samples.resolve("MPEG-4/video.mp4");
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        String ffmpegHome = System.getProperty("FFMPEG_BIN");
+        if (ffmpegHome == null) {
+            ffmpegHome = System.getenv("FFMPEG_BIN");
+        }
+        Assert.assertNotNull("Nor command line property, neither system variable FFMPEG_BIN is set up", ffmpegHome);
+        bin = Paths.get(ffmpegHome);
+
+        Assert.assertTrue("Sample videos weren't found: " + samples.toAbsolutePath(), Files.exists(samples));
+    }
 
     @Test
     public void testShowStreams() throws Exception {
         FFprobeType result = FFprobe.atPath(bin)
-                .setInputPath(input)
+                .setInputPath(videoMP4)
                 .setShowStreams(true)
                 .execute();
 
@@ -27,7 +42,7 @@ public class FFprobeTest {
     @Test
     public void testSelectStreamWithShowStreams() throws Exception {
         FFprobeType result = FFprobe.atPath(bin)
-                .setInputPath(input)
+                .setInputPath(videoMP4)
                 .setShowStreams(true)
                 .setSelectStreams(new StreamSpecifier("v"))
                 .execute();
@@ -42,7 +57,7 @@ public class FFprobeTest {
     @Test
     public void testSelectStreamWithShowPackets() throws Exception {
         FFprobeType result = FFprobe.atPath(bin)
-                .setInputPath(input)
+                .setInputPath(videoMP4)
                 .setShowPackets(true)
                 .setSelectStreams(new StreamSpecifier(5))
                 .execute();
