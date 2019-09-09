4f1f2d5ecf918cc4dd5242faa0145e9203f41f65
Denis Kokorin <kokorin86@gmail.com>
2019-09-09 20:43:15 +0300

    FFPROBE SeekableByteChannel: WIP

---
+++ src/main/java/com/github/kokorin/jaffree/ffprobe/PipeInput.java
@@ -0,0 +1,58 @@
+/*
+ *    Copyright  2019 Denis Kokorin
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package com.github.kokorin.jaffree.ffprobe;
+
+import com.github.kokorin.jaffree.util.IOUtil;
+import com.github.kokorin.jaffree.util.SocketOutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class PipeInput extends SocketInput {
+    private final InputStream inputStream;
+    private final int bufferSize;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PipeInput.class);
+
+    public PipeInput(InputStream inputStream, int bufferSize) {
+        super("tcp");
+        this.inputStream = inputStream;
+        this.bufferSize = bufferSize;
+    }
+
+    @Override
+    Negotiator negotiator() {
+        return new Negotiator() {
+            @Override
+            public void negotiateAndClose(ServerSocket serverSocket) throws IOException {
+                LOGGER.debug("Accepting connection: {}", serverSocket);
+
+                try (Socket socket = serverSocket.accept();
+                     OutputStream outputStream = new SocketOutputStream(serverSocket, socket)) {
+                    LOGGER.debug("Connection accepted, copying");
+                    IOUtil.copy(inputStream, outputStream, bufferSize);
+                }
+            }
+        };
+    }
+}
