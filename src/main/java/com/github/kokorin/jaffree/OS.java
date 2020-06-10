1e9f935af4c383c68c7798f8998f05ef8a30caf7
vicnevicne <vicnevicne@gmail.com>
2020-06-10 21:47:24 +0200

    (refactoring)

--- src/main/java/com/github/kokorin/jaffree/OS.java
+++ src/main/java/com/github/kokorin/jaffree/OS.java
@@ -19,7 +19,9 @@ package com.github.kokorin.jaffree;
 
 public class OS {
     // Test and values taken from org.apache.commons.lang3.SystemUtils
-    public static final boolean IS_WINDOWS = System.getProperty("os.name").startsWith("Windows");
-    public static final boolean IS_MAC = System.getProperty("os.name").startsWith("Mac");
-    public static final boolean IS_LINUX = System.getProperty("os.name").startsWith("Linux") || System.getProperty("os.name").startsWith("LINUX");
+    public static final String OS_NAME = System.getProperty("os.name");
+
+    public static final boolean IS_WINDOWS = OS_NAME.startsWith("Windows");
+    public static final boolean IS_MAC = OS_NAME.startsWith("Mac");
+    public static final boolean IS_LINUX = OS_NAME.startsWith("Linux") || OS_NAME.startsWith("LINUX");
 }
