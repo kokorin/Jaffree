73129a73a9e6f08aea8efbe43d655dc9ba8c03bc
vicnevicne <vicnevicne@gmail.com>
2020-06-12 01:33:33 +0200

    Updated license boilerplate

--- src/test/java/examples/ffmpeg/DesktopCapture.java
+++ src/test/java/examples/ffmpeg/DesktopCapture.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright  2019 Denis Kokorin
+ *    Copyright  2020 Vicne
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
