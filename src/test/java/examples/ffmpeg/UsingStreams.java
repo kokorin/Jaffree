1dc15676902b950e21de2045fe6730e2df02d799
kokorin <9crqUVAXd6Q17EY354lkbYeB>
2021-02-12 10:01:01 +0200

    Readme & examples

---
+++ src/test/java/examples/ffmpeg/UsingStreams.java
@@ -0,0 +1,39 @@
+package examples.ffmpeg;
+
+import com.github.kokorin.jaffree.ffmpeg.FFmpeg;
+import com.github.kokorin.jaffree.ffmpeg.PipeInput;
+import com.github.kokorin.jaffree.ffmpeg.PipeOutput;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class UsingStreams {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Exactly 2 arguments expected: path to source and destination media files");
+            System.exit(1);
+        }
+
+        Path pathToSrc = Paths.get(args[0]);
+        Path pathToDst = Paths.get(args[1]);
+
+        try (InputStream inputStream =
+                     Files.newInputStream(pathToSrc);
+             OutputStream outputStream =
+                     Files.newOutputStream(pathToDst, StandardOpenOption.CREATE,
+                             StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING)
+        ) {
+            FFmpeg.atPath()
+                    .addInput(PipeInput.pumpFrom(inputStream))
+                    .addOutput(
+                            PipeOutput.pumpTo(outputStream)
+                                    .setFormat("flv")
+                    )
+                    .execute();
+        }
+    }
+}
--- src/test/java/examples/ffprobe/ShowStreams.java
+++ src/test/java/examples/ffprobe/ShowStreams.java
@@ -1,97 +1,30 @@
 package examples.ffprobe;
 
-import com.github.kokorin.jaffree.ffmpeg.FFmpeg;
-import com.github.kokorin.jaffree.ffmpeg.FFmpegProgress;
-import com.github.kokorin.jaffree.ffmpeg.FFmpegResult;
-import com.github.kokorin.jaffree.ffmpeg.NullOutput;
-import com.github.kokorin.jaffree.ffmpeg.ProgressListener;
-import com.github.kokorin.jaffree.ffmpeg.UrlInput;
 import com.github.kokorin.jaffree.ffprobe.FFprobe;
 import com.github.kokorin.jaffree.ffprobe.FFprobeResult;
 import com.github.kokorin.jaffree.ffprobe.Stream;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class ShowStreams {
-    private final String ffmpegBin;
-    private final String video;
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ShowStreams.class);
-
-    public ShowStreams(String ffmpegBin, String video) {
-        this.ffmpegBin = ffmpegBin;
-        this.video = video;
-    }
-
-    public void execute() {
-        FFprobe ffprobe;
-        if (ffmpegBin != null) {
-            ffprobe = FFprobe.atPath(Paths.get(ffmpegBin));
-        } else {
-            ffprobe = FFprobe.atPath();
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Exactly 1 argument expected: path to media file");
+            System.exit(1);
         }
 
-        FFprobeResult result = ffprobe
+        String pathToVideo = args[0];
+
+        FFprobeResult result = FFprobe.atPath()
                 .setShowStreams(true)
-                .setInput(video)
+                .setInput(pathToVideo)
                 .execute();
 
         for (Stream stream : result.getStreams()) {
-            System.out.println("Stream " + stream.getIndex() + " type " + stream.getCodecType() + " duration " + stream.getDuration(TimeUnit.SECONDS));
+            System.out.println("Stream #" + stream.getIndex()
+                    + " type: " + stream.getCodecType()
+                    + " duration: " + stream.getDuration(TimeUnit.SECONDS) + " seconds");
         }
 
-        FFmpeg ffmpeg;
-        if (ffmpegBin != null) {
-            ffmpeg = FFmpeg.atPath(Paths.get(ffmpegBin));
-        } else {
-            ffmpeg = FFmpeg.atPath();
-        }
-
-        final AtomicLong durationMillis = new AtomicLong();
-        FFmpegResult fFmpegResult = ffmpeg
-                .addInput(
-                        UrlInput.fromUrl(video)
-                )
-                .addOutput(new NullOutput())
-                .setProgressListener(new ProgressListener() {
-                    @Override
-                    public void onProgress(FFmpegProgress progress) {
-                        durationMillis.set(progress.getTimeMillis());
-                    }
-                })
-                .execute();
-
-        System.out.println("Exact duration: " + durationMillis.get() + " milliseconds");
-
-    }
-
-    public static void main(String[] args) {
-        Iterator<String> argIter = Arrays.asList(args).iterator();
-
-        String ffmpegBin = null;
-        String video = null;
-
-        while (argIter.hasNext()) {
-            String argName = argIter.next();
-
-            if (!argIter.hasNext()) {
-                video = argName;
-                break;
-            }
-
-            String argValue = argIter.next();
-
-            if ("-ffmpeg_bin".equals(argName)) {
-                ffmpegBin = argValue;
-            }
-        }
-
-        new ShowStreams(ffmpegBin, video).execute();
     }
 }
