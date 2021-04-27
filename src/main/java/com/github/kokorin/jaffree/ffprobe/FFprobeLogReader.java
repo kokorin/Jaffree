f6c390ba6b644e82d352b651e66577e6a94de0e2
kokorin <9crqUVAXd6Q17EY354lkbYeB>
2021-04-27 09:49:05 +0300

    Checkstyle: FFprobeLogReader

--- src/main/java/com/github/kokorin/jaffree/ffprobe/FFprobeLogReader.java
+++ src/main/java/com/github/kokorin/jaffree/ffprobe/FFprobeLogReader.java
@@ -13,14 +13,25 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+/**
+ * {@link FFprobeLogReader} reads ffprobe output (from stderr), parses {@link FFprobeResult} and
+ * logs and sends logs to SLF4J with corresponding log level.
+ */
 public class FFprobeLogReader implements StdReader<FFprobeResult> {
     private static final Logger LOGGER = LoggerFactory.getLogger(FFprobeLogReader.class);
 
+    /**
+     * Reads ffprobe output from provided {@link InputStream} and parses {@link FFprobeResult} and
+     * logs.
+     * @param inputStream input stream to read from
+     * @return parsed {@link FFprobeResult}
+     */
+    @SuppressWarnings("checkstyle:MissingSwitchDefault")
     @Override
-    public FFprobeResult read(InputStream stdOut) {
+    public FFprobeResult read(final InputStream inputStream) {
         LogMessageIterator logMessageIterator = new LogMessageIterator(
                 new LineIterator(
-                        new BufferedReader(new InputStreamReader(stdOut))
+                        new BufferedReader(new InputStreamReader(inputStream))
                 )
         );
 
@@ -54,7 +65,8 @@ public class FFprobeLogReader implements StdReader<FFprobeResult> {
                 LOGGER.info(logMessage.message);
             }
 
-            if (logMessage.logLevel != null && logMessage.logLevel.code() <= LogLevel.ERROR.code()) {
+            if (logMessage.logLevel != null
+                    && logMessage.logLevel.code() <= LogLevel.ERROR.code()) {
                 if (errorMessage == null) {
                     errorMessage = logMessage.message;
                 } else {
