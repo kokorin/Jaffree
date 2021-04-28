8b9b5ba4cd6357b935e30e82924539cf7ddc5a27
kokorin <9crqUVAXd6Q17EY354lkbYeB>
2021-04-28 08:48:34 +0300

    Checkstyle: add license check, create log package

--- src/main/java/com/github/kokorin/jaffree/log/LogMessage.java
+++ src/main/java/com/github/kokorin/jaffree/log/LogMessage.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2020-2021 Denis Kokorin
+ *    Copyright 2021 Denis Kokorin
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -15,7 +15,26 @@
  *
  */
 
+package com.github.kokorin.jaffree.log;
+
+import com.github.kokorin.jaffree.LogLevel;
+
 /**
- * Base package for Jaffree project.
+ * ffprobe/ffmpeg log message with log level.
  */
-package com.github.kokorin.jaffree;
+@SuppressWarnings("checkstyle:VisibilityModifier")
+public class LogMessage {
+    public final LogLevel logLevel;
+    public final String message;
+
+    /**
+     * Creates {@link LogMessage}.
+     *
+     * @param logLevel log level
+     * @param message  message
+     */
+    public LogMessage(final LogLevel logLevel, final String message) {
+        this.logLevel = logLevel;
+        this.message = message;
+    }
+}
