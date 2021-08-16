7b395115150f941e333b232a0fadeb73941fb727
Denis Kokorin <kokorin86@gmail.com>
2021-08-16 18:29:32 +0300

    Fix after review.

--- src/test/java/com/github/kokorin/jaffree/Config.java
+++ src/test/java/com/github/kokorin/jaffree/Config.java
@@ -1,24 +1,29 @@
 package com.github.kokorin.jaffree;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Config {
-    public static final Path FFMPEG_BIN = initFfmegBin();
+    private static final Logger LOGGER = LoggerFactory.getLogger(Config.class);
+    public static final Path FFMPEG_BIN = getFfmpegBin();
 
-    private static Path initFfmegBin() {
+    private static Path getFfmpegBin() {
         String ffmpegHome = System.getProperty("FFMPEG_BIN");
         if (ffmpegHome != null) {
-            System.out.println("Using FFMPEG_BIN from system property: '" + ffmpegHome + "'");
+            LOGGER.info("Using FFMPEG_BIN from system property: {}", ffmpegHome);
             return Paths.get(ffmpegHome);
         }
+
         ffmpegHome = System.getenv("FFMPEG_BIN");
         if (ffmpegHome != null) {
-            System.out.println("Using FFMPEG_BIN from environment variable: '" + ffmpegHome + "'");
+            LOGGER.info("Using FFMPEG_BIN from environment variable: {}", ffmpegHome);
             return Paths.get(ffmpegHome);
-        } else {
-            System.out.println("FFMPEG_BIN not configured: expecting to find ffmpeg on PATH");
-            return null;
         }
+
+        LOGGER.warn("FFMPEG_BIN not configured: expecting to find ffmpeg on PATH");
+        return null;
     }
 }
