26ec7618dc1880b9358053cdda646a8ad086c5e9
Cromefire_ <cromefire_@outlook.com>
2021-08-29 20:36:34 +0200

    Added author to copyright

--- src/main/java/com/github/kokorin/jaffree/ffmpeg/FFmpegProgressReader.java
+++ src/main/java/com/github/kokorin/jaffree/ffmpeg/FFmpegProgressReader.java
@@ -1,5 +1,6 @@
 /*
  *    Copyright 2021 Denis Kokorin
+ *    Copyright 2021 Cromefire_
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
