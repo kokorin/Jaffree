24fa4ee3db3f8cb0329dfe046637448719e6f5b8
Cromefire_ <cromefire_@outlook.com>
2021-08-30 09:45:12 +0200

    Fixed license style

--- src/main/java/com/github/kokorin/jaffree/ffmpeg/FFmpegProgressReader.java
+++ src/main/java/com/github/kokorin/jaffree/ffmpeg/FFmpegProgressReader.java
@@ -1,6 +1,5 @@
 /*
- *    Copyright 2021 Denis Kokorin
- *    Copyright 2021 Cromefire_
+ *    Copyright 2021 Denis Kokorin, Cromefire_
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
